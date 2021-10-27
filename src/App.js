07ea5804966a977e466e76eb6ccf78ed076a8422
KimGenius <geniusk1047@naver.com>
2021-10-27 14:49:04 +0900

    게시글 생성 연동 & 나름의 디자인 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import './App.css';
-import {Button, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-import {useEffect, useState} from "react";
-import axios from "axios";
+import './App.css'
+import {Button, Table, TableBody, TableCell, TableHead, TableRow, TextField} from "@mui/material"
+import {useEffect, useState} from "react"
+import axios from "axios"
+import dayjs from "dayjs"
 
 function App() {
+  let [name, setName] = useState([])
+  let [content, setContent] = useState([])
   let [boards, setBoards] = useState([])
   useEffect(() => {
     getBoards()
   }, [])
 
-  // 통신 메서드
   async function getBoards() {
     const url = "http://localhost:5000/boards"
     const { data } = await axios.get(url)
     setBoards(data)
   }
+
+  async function createBoard() {
+    const url = "http://localhost:5000/boards"
+    await axios.post(url, {
+      name, content
+    })
+    getBoards()
+  }
   return (
     <div className="App">
-      <Button>게시글 등록</Button>
+      <TextField id="standard-basic" label="이름" value={name} onChange={e => setName(e.target.value)}/>
+      <TextField id="standard-basic" label="내용" value={content} onChange={e => setContent(e.target.value)} />
+      <Button onClick={() => createBoard()}>게시글 등록</Button>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -36,13 +48,13 @@ function App() {
                 {board.name}
               </TableCell>
               <TableCell align="right">{board.content}</TableCell>
-              <TableCell align="right">{board.createdAt}</TableCell>
+              <TableCell align="right">{dayjs(board.createdAt).format('YYYY-MM-DD HH:mm:ss')}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
