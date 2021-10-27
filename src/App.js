80292e86d570480be188062fb23fa649dc9eb004
KimGenius <geniusk1047@naver.com>
2021-10-27 10:55:18 +0900

    backend 리스트 가져오기 까지 연동

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
 import './App.css';
 import {Button, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  let [boards, setBoards] = useState([])
+  useEffect(() => {
+    getBoards()
+  }, [])
+
+  // 통신 메서드
+  async function getBoards() {
+    const url = "http://localhost:5000/boards"
+    const { data } = await axios.get(url)
+    setBoards(data)
+  }
   return (
     <div className="App">
       <Button>게시글 등록</Button>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell align="right">내용</TableCell>
+            <TableCell align="right">작성일</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {boards.map((board) => (
             <TableRow
-              key={row.name}
+              key={board._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {board.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{board.content}</TableCell>
+              <TableCell align="right">{board.createdAt}</TableCell>
             </TableRow>
           ))}
         </TableBody>
